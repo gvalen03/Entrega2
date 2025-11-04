ffe5fff7058e70dbf96ec6728812176d17f59588
gvalen03 <gloriaidrobo@unicauca.edu.co>
2025-11-04 01:01:12 -0500

    add word validator

--- document-microservice/src/main/java/co/edu/unicauca/document_microservice/factory/DocumentoFactory.java
+++ document-microservice/src/main/java/co/edu/unicauca/document_microservice/factory/DocumentoFactory.java
@@ -2,35 +2,53 @@ package co.edu.unicauca.document_microservice.factory;
 
 import co.edu.unicauca.document_microservice.utilities.validator.DocumentoValidator;
 import co.edu.unicauca.document_microservice.utilities.validator.PdfValidator;
+import co.edu.unicauca.document_microservice.utilities.validator.WordValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
 public class DocumentoFactory {
 
-    private static volatile DocumentoFactory instance;
+    private final Map<String, List<DocumentoValidator>> validators = new HashMap<>();
 
-    private DocumentoFactory() {}
+    @Autowired
+    public DocumentoFactory(List<DocumentoValidator> validatorList) {
 
-    public static DocumentoFactory getInstance() {
-        if (instance == null) {
-            synchronized (DocumentoFactory.class) {
-                if (instance == null) {
-                    instance = new DocumentoFactory();
-                }
-            }
-        }
-        return instance;
+        validators.put("FORMATO_A", List.of(
+                getValidator(validatorList, PdfValidator.class),
+                getValidator(validatorList, WordValidator.class)
+        ));
+
+        validators.put("ANTEPROYECTO", List.of(
+                getValidator(validatorList, PdfValidator.class)
+        ));
+
+        validators.put("CARTA_EMPRESA", List.of(
+                getValidator(validatorList, PdfValidator.class)
+        ));
+
+        validators.put("INFORME_FINAL", List.of(
+                getValidator(validatorList, WordValidator.class)
+        ));
     }
 
-    public DocumentoValidator crearValidator(String tipoDocumento) {
-        switch (tipoDocumento) {
-            case "FORMATO_A":
-            case "ANTEPROYECTO":
-            case "CARTA_EMPRESA":
-            case "MONOGRAFIA":
-            case "ANEXOS":
-            case "PRESENTACION":
-                return new PdfValidator();
-            default:
-                throw new IllegalArgumentException("Tipo de documento no soportado: " + tipoDocumento);
+    private DocumentoValidator getValidator(List<DocumentoValidator> validatorList,
+                                            Class<? extends DocumentoValidator> clazz) {
+        return validatorList.stream()
+                .filter(clazz::isInstance)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Validator no encontrado: " + clazz.getSimpleName()));
+    }
+
+    public List<DocumentoValidator> crearValidators(String tipoDocumento) {
+        List<DocumentoValidator> lista = validators.get(tipoDocumento);
+        if (lista == null || lista.isEmpty()) {
+            throw new IllegalArgumentException("Tipo de documento no soportado: " + tipoDocumento);
         }
+        return lista;
     }
 }
--- document-microservice/src/main/java/co/edu/unicauca/document_microservice/utilities/validator/PdfValidator.java
+++ document-microservice/src/main/java/co/edu/unicauca/document_microservice/utilities/validator/PdfValidator.java
@@ -13,3 +13,4 @@ public class PdfValidator implements DocumentoValidator {
         }
     }
 }
+
